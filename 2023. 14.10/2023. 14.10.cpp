aab4d7c1f3aaa27ccb4388c68fda24929e126094
Ostap4i <137522973+Ostap4i@users.noreply.github.com>
2023-10-14 13:26:36 +0300

    Add files via upload

---
+++ 2023. 14.10/2023. 14.10.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+// Функція для порівняння двох стеків
+bool compareStacks(stack<int>& stack1, stack<int>& stack2) {
+    if (stack1.size() != stack2.size()) {
+        return false;
+    }
+
+    while (!stack1.empty()) {
+        if (stack1.top() != stack2.top()) {
+            return false;
+        }
+        stack1.pop();
+        stack2.pop();
+    }
+
+    return true;
+}
+
+// Функція для виводу стека
+void printStack(stack<int> st) {
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    stack<int> stack1, stack2;
+
+    // Додавання елементів у перший стек
+    stack1.push(1);
+    stack1.push(2);
+    stack1.push(3);
+
+    // Додавання елементів у другий стек
+    stack2.push(1);
+    stack2.push(2);
+    stack2.push(3);
+
+    cout << "Stack 1 for comparison: ";
+    printStack(stack1);
+    cout << "Stack 2 for comparison: ";
+    printStack(stack2);
+
+    if (compareStacks(stack1, stack2)) {
+        // Якщо стеки однакові, здійснити інверсію одного з них
+        stack<int> invertedStack = stack1;
+        while (!invertedStack.empty()) {
+            invertedStack.top() *= -1;
+            invertedStack.pop();
+        }
+        cout << "Stack 1 after inversion:";
+        printStack(invertedStack);
+    }
+    else {
+        // Якщо стеки різні, створити новий стек з від'ємних елементів
+        stack<int> negativeStack;
+        while (!stack1.empty() && !stack2.empty()) {
+            negativeStack.push(-(stack1.top() + stack2.top()));
+            stack1.pop();
+            stack2.pop();
+        }
+        cout << "New stack with negative elements: ";
+        printStack(negativeStack);
+    }
+
+    return 0;
+}
---
+++ stack/stack.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    // Створення стеку
+    std::stack<int> myStack;
+
+    // Додавання елементів до стеку
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+
+    // Виведення значень елементів на екран
+    cout << "Stack element value:" << endl;
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+
+    // Виконання дій
+    // Наприклад, додамо ще елементи
+    myStack.push(6);
+    myStack.push(7);
+
+    // Виведення результатів
+   cout << "\nNew stack element values:" << endl;
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+
+    return 0;
+}
